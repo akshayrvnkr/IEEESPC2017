512a7e88999c11cb18cb2baaab4ef9a015875dbe
Deepesh Agarawal <deepesh.agarawal@gmail.com>
2016-11-01 01:08:26 +0530

    getperiod... check it out though wasnt able to get the terminology used...

--- RT_FFT/getperiod.py
+++ RT_FFT/getperiod.py
@@ -8,7 +8,7 @@ def getperiod(acf=None, wv=None, timesig=None, step=None, pmin=None, pmax=None):
 
         for i in range(pmin-1,pmax - 1-1):        # maximum beat period
             for a in range(0,numelem-1):            # number of comb elements
-                for b in range[1 - a,a - 1]:                # gs using normalization of comb elements
+                for b in range(1 - a,a - 1):                # gs using normalization of comb elements
                     rcf[i] = rcf[i] + (acf[a * i + b] * wv[i]) / (2 * a - 1)
 
     else:
@@ -16,5 +16,5 @@ def getperiod(acf=None, wv=None, timesig=None, step=None, pmin=None, pmax=None):
 
         for i in range(pmin-1,pmax - 1-1):        # maximum beat period
             for a in range(0,numelem-1):            # number of comb elements
-                for b in range[1 - a,a - 1]:                # cds not normalizing comb elements
+                for b in range(1 - a,a - 1):                # cds not normalizing comb elements
                     rcf[i]= rcf[i] + acf[a * i + b] * wv[i]
